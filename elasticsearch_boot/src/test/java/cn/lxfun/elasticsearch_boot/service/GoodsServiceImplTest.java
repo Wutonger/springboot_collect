a89cdebed68e6b545532b248c003daae3e57cc05
loux <1176160102@qq.com>
2020-01-19 20:32:23 +0800

    增加test类

---
+++ elasticsearch_boot/src/test/java/cn/lxfun/elasticsearch_boot/service/GoodsServiceImplTest.java
@@ -0,0 +1,16 @@
+package cn.lxfun.elasticsearch_boot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoodsServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
